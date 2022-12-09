6781ae1af4e8eb1078907c3c888e36e4c51c68e1
zxc10110 <kannyasaenkham@gmail.com>
2022-12-09 16:37:46 +0700

    edit queue for user

--- frontend/pages/api/queue/editQueue.js
+++ frontend/pages/api/queue/editQueue.js
@@ -1,23 +1,23 @@
 import { supabase } from "../supabase"
 
-//update from server
+//edit from client
 export default async function handler(req, res) {
 
   //call parameter from body
   //"queue_date": "2022-12-09T07:36:58.793+00:00"
-  const {queue_id, status} = req.body
+  const {queue_id, queue_date} = req.body
 
   //check if queue id exist
   //insert user_id noes not exist 
   const { er } = await supabase.from('queue').update([
     {
+      queue_date: queue_date,
       update_date: new Date(),
-      status: status,
     }
   ]).eq('queue_id', queue_id)
   //check error
   if (er) throw er
-  console.log("Update Data Success!")
+  console.log("Edit Data Success!")
 
   //query data 
   const { data, error } = await supabase.from('queue').select().eq('queue_id', queue_id)
