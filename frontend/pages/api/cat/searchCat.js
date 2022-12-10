d5590881512fe47002848a6cc712344ea021e4ae
kanticha954 <kanticha.tri@gmail.com>
2022-12-10 23:13:19 +0700

    search fin?

--- frontend/pages/api/cat/searchCat.js
+++ frontend/pages/api/cat/searchCat.js
@@ -3,19 +3,24 @@ import { useEffect, useState } from 'react'
 
 export default async function handler(req, res) {
 
-    // const { cat_id} = req.body
-    // const { sex} = req.body
-    // const { shelter_id } = req.body
-    // const { status } = req.body
-
-    const { cat_id, sex, shelter_id, status} = req.body
+    const { cat_id, cat_name, sex, breed, color, sterile, vaccine, age, status, shelter_id} = req.body
 
     let query = supabase
         .from('cat_profile')
-        .select('cat_id, cat_name,sex, breed, color, detail, shelter_id')
-
+        .select()
+        /* select ตามที่โชว์
+        .select('cat_id, cat_name, sex, breed, color, cat_picture, detail, shelter_id')
+        */
+       //.select('cat_id, cat_name,sex, breed, color, detail, shelter_id, shelter_profile!inner(*)')
+        
         if(cat_id) { query = query.eq('cat_id', cat_id)}
         if (sex) { query = query.eq('sex', sex) }
+        if (breed) { query = query.eq('breed', breed) }
+        if (color) { query = query.eq('color', color) }
+        if (sterile) { query = query.eq('sterile', sterile) }
+        if (cat_name) { query = query.eq('cat_name', cat_name) }
+        if (vaccine) { query = query.eq('vaccine', vaccine) }
+        if (age) { query = query.eq('age', age) }
         if (status) { query = query.eq('status', status) }
         if (shelter_id) { query = query.eq('shelter_id', shelter_id) }
         
