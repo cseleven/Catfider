20d789185d22bb1302ee71b87111207a8e67a6d9
jjjjjjj7777777 <jee3848@gmail.com>
2022-12-11 16:32:14 +0700

    y

---
+++ frontend/middleware.js
@@ -0,0 +1,37 @@
+import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
+
+export async function middleware(req) {
+  let res = NextResponse.next()
+
+  const supabase = createMiddlewareSupabaseClient({ req, res })
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  if(!session) {
+    console.log("Authorization error, redirecting to login page")
+    res = NextResponse.redirect(new URL('/signin', req.url))
+  } 
+
+  if(req.nextUrl.pathname.startsWith('/user')) {
+    if(session.user.user_metadata.role!=1){
+        console.log("you not user")
+        res = NextResponse.redirect(new URL('/', req.url))
+    }
+  }
+
+  if(req.nextUrl.pathname.startsWith('/shelter')) {
+    if(session.user.user_metadata.role!=2){
+        console.log("you not shelter")
+        res = NextResponse.redirect(new URL('/', req.url))
+    }
+  }
+
+  return res
+}
+
+export const config = {
+  matcher: ['/user/:path*','/shelter/:path*'],
+}
