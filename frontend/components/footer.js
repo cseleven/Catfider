7e3de671020fa5b70aa5af0ef52aa67a1add9903
Tharita <63050147@kmitl.ac.th>
2022-12-14 02:57:51 +0700

    add fixed nav bar always stay at bottom of page

--- frontend/components/footer.js
+++ frontend/components/footer.js
@@ -1,22 +1,23 @@
 export default function Footer() {
     return (
-      <footer class="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://github.com/cseleven" class="hover:underline">Eleven</a>. SE, Computer Science, KMITL.
-        </span>
-        <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
-            <li>
-                <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
-            </li>
-            <li>
-                <a href="https://github.com/cseleven/Catfider-Front.git" class="mr-4 hover:underline md:mr-6">Frontend</a>
-            </li>
-            <li>
-                <a href="https://github.com/cseleven/Catfinder-Back.git" class="mr-4 hover:underline md:mr-6">Backend</a>
-            </li>
-            <li>
-                <a href="https://github.com/cseleven" class="hover:underline">Contact</a>
-            </li>
-        </ul>
-      </footer>
+
+        <footer class="fixed inset-x-0 bottom-0 p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://github.com/cseleven" class="hover:underline">Eleven</a>. SE, Computer Science, KMITL.
+            </span>
+            <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+                <li>
+                    <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
+                </li>
+                <li>
+                    <a href="https://github.com/cseleven/Catfider-Front.git" class="mr-4 hover:underline md:mr-6">Frontend</a>
+                </li>
+                <li>
+                    <a href="https://github.com/cseleven/Catfinder-Back.git" class="mr-4 hover:underline md:mr-6">Backend</a>
+                </li>
+                <li>
+                    <a href="https://github.com/cseleven" class="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </footer>
     );
 }
--- frontend/pages/shelter/shelter-form.js
+++ frontend/pages/shelter/shelter-form.js
@@ -1,143 +1,213 @@
 import Signin from '../../components/sigin'
 
 export default function ShelterForm() {
-  return (
-    <div class="h-[87vh]">
-        <div class="w-screen h-[20rem]">
-            <p class="text-[48px] font-normal text-center pt-[7rem]">สมัครสมาชิก</p>
-            <p class="text-[20px] font-normal text-center pt-2">สำหรับมูลนิธิ</p>
+    return (
+        <div class="h-[87vh]">
+            <div class="w-screen h-[20rem]">
+                <p class="text-[48px] font-normal text-center pt-[7rem]">สมัครสมาชิก</p>
+                <p class="text-[20px] font-normal text-center pt-2">สำหรับมูลนิธิ</p>
+            </div>
+
+            <form action="#" method="POST">
+                <div class="bg-gray-200">
+                    <div class="container mx-auto flex justify-around">
+
+                        <div class="w-1/3 pt-9">
+                            <h4 class="font-inter font-medium">ข้อมูลมูลนิธิ</h4>
+                            <h6 class="font-light pt-2">ข้อมูลเบื้องต้นของมูลนิธิเพื่อให้ทางทีมงาน Cat Finder และ ผู้ใช้งานทั่วไปสามารถอ่านข้อมูล</h6>
+                        </div>
+
+                        <div class="w-3/5 my-8 bg-white shadow rounded-t-md">
+                            <div class="p-7 max-w-lg">
+                                <div class="grid grid-cols-1 gap-6 pb-5">
+                                    <label class="block">
+                                        <span class="font-inter text-gray-700">ชื่อมูลนิธิ</span>
+                                        <input
+                                            type="text"
+                                            class="
+                                            mt-1
+                                            block
+                                            w-full
+                                            rounded-md
+                                            border-gray-300
+                                            shadow-sm
+                                            focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+                                        "
+                                            placeholder=""
+                                        />
+                                    </label>
+                                    <label class="block">
+                                        <span class="font-inter text-gray-700">เว็บไซต์</span>
+                                        <input
+                                            type="text"
+                                            class="
+                                            mt-1
+                                            block
+                                            w-full
+                                            rounded-md
+                                            border-gray-300
+                                            shadow-sm
+                                            focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+                                        "
+                                            placeholder=""
+                                        />
+                                    </label>
+                                    <label class="block">
+                                        <span class="font-inter text-gray-700">ชื่อธนาคาร (1)</span>
+                                        <input
+                                            type="text"
+                                            class="
+                                            mt-1
+                                            block
+                                            w-full
+                                            rounded-md
+                                            border-gray-300
+                                            shadow-sm
+                                            focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+                                        "
+                                            placeholder=""
+                                        />
+                                    </label>
+                                    <label class="block">
+                                        <span class="font-inter text-gray-700">ชื่อธนาคาร (2)</span>
+                                        <input
+                                            type="text"
+                                            class="
+                                            mt-1
+                                            block
+                                            w-full
+                                            rounded-md
+                                            border-gray-300
+                                            shadow-sm
+                                            focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+                                        "
+                                            placeholder=""
+                                        />
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+
+            <form action="#" method="POST">
+                <div class="bg-gray-200">
+                    <div class="container mx-auto flex justify-around">
+
+                        <div class="w-1/3 pt-9">
+                            <h4 class="font-inter font-medium">ข้อมูลสำหรับติดต่อ</h4>
+                            <h6 class="font-light pt-2">ข้อมูลสำหรับให้ทางทีมงาน cat Finder ติดต่อกับทางมูลนิธิ</h6>
+                        </div>
+
+                        <div class="w-3/5 my-8 bg-white shadow rounded-t-md">
+                            <div class="p-7 max-w-lg">
+                                <div class="grid grid-cols-1 gap-6 pb-5">
+                                    <label class="block">
+                                        <span class="font-inter text-gray-700">ชื่อจริง</span>
+                                        <input
+                                            type="text"
+                                            class="
+                                            mt-1
+                                            block
+                                            w-full
+                                            rounded-md
+                                            border-gray-300
+                                            shadow-sm
+                                            focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+                                        "
+                                            placeholder=""
+                                        />
+                                    </label>
+                                    <label class="block">
+                                        <span class="font-inter text-gray-700">Email address</span>
+                                        <input
+                                            type="text"
+                                            class="
+                                            mt-1
+                                            block
+                                            w-full
+                                            rounded-md
+                                            border-gray-300
+                                            shadow-sm
+                                            focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+                                        "
+                                            placeholder=""
+                                        />
+                                    </label>
+                                    <label class="block">
+                                        <span class="font-inter text-gray-700">เบอร์ติดต่อ</span>
+                                        <input
+                                            type="text"
+                                            class="
+                                            mt-1
+                                            block
+                                            w-full
+                                            rounded-md
+                                            border-gray-300
+                                            shadow-sm
+                                            focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+                                        "
+                                            placeholder=""
+                                        />
+                                    </label>
+                                    <label class="block">
+                                        <span class="font-inter text-gray-700">ที่อยู่มูลนิธิ</span>
+                                        <input
+                                            type="text"
+                                            class="
+                                            mt-1
+                                            block
+                                            w-full
+                                            rounded-md
+                                            border-gray-300
+                                            shadow-sm
+                                            focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+                                        "
+                                            placeholder=""
+                                        />
+                                    </label>
+                                    <label class="block">
+                                        <span class="font-inter text-gray-700">อำเภอ</span>
+                                        <input
+                                            type="text"
+                                            class="
+                                            mt-1
+                                            block
+                                            w-full
+                                            rounded-md
+                                            border-gray-300
+                                            shadow-sm
+                                            focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+                                        "
+                                            placeholder=""
+                                        />
+                                    </label>
+                                    <label class="block">
+                                        <span class="font-inter text-gray-700">ลิงก์ google map (https://)</span>
+                                        <input
+                                            type="text"
+                                            class="
+                                            mt-1
+                                            block
+                                            w-full
+                                            rounded-md
+                                            border-gray-300
+                                            shadow-sm
+                                            focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
+                                        "
+                                            placeholder=""
+                                        />
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </form>
+
         </div>
 
-        <form action="#" method="POST">
-            <div class="bg-gray-200">
-                <div class="container mx-auto flex justify-around">
-                    <div class="w-1/3 pt-9">
-                        <h4 class="font-inter font-medium">ข้อมูลมูลนิธิ</h4>
-                        <h6 class="font-light pt-2">ข้อมูลเบื้องต้นของมูลนิธิเพื่อให้ทางทีมงาน Cat Finder และ ผู้ใช้งานทั่วไปสามารถอ่านข้อมูล</h6>
-                    </div>
-                    <div class="w-3/5 my-8 bg-white shadow rounded-t-md">
-                        <div class="p-7 max-w-lg">
-                            <div class="grid grid-cols-1 gap-6 pb-5">
-                                <label class="block">
-                                    <span class="font-inter text-gray-700">Email address</span>
-                                    <input
-                                        type="text"
-                                        class="
-                                            mt-1
-                                            block
-                                            w-full
-                                            rounded-md
-                                            border-gray-300
-                                            shadow-sm
-                                            focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
-                                        "
-                                        placeholder=""
-                                    />
-                                </label>
-                                <label class="block">
-                                    <span class="font-inter text-gray-700">Password</span>
-                                    <input
-                                        type="text"
-                                        class="
-                                            mt-1
-                                            block
-                                            w-full
-                                            rounded-md
-                                            border-gray-300
-                                            shadow-sm
-                                            focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
-                                        "
-                                        placeholder=""
-                                    />
-                                </label>
-                                <label class="block">
-                                    <span class="font-inter text-gray-700">Confirm Password</span>
-                                    <input
-                                        type="text"
-                                        class="
-                                            mt-1
-                                            block
-                                            w-full
-                                            rounded-md
-                                            border-gray-300
-                                            shadow-sm
-                                            focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
-                                        "
-                                        placeholder=""
-                                    />
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                </div>
 
-                {/* form 2 */}
-                <div class="container mx-auto flex justify-around">
-                    <div class="w-1/3 pt-9">
-                        <h4 class="font-inter font-medium">ข้อมูลสำหรับติดต่อ</h4>
-                        <h6 class="font-light pt-2">ข้อมูลสำหรับให้ทางทีมงาน at Finder ติดต่อกับทางมูลนิธิ</h6>
-                    </div>
-                    <div class="w-3/5 my-8 bg-white shadow rounded-t-md">
-                        <div class="p-7 max-w-lg">
-                            <div class="grid grid-cols-1 gap-6 pb-5">
-                                <label class="block">
-                                    <span class="font-inter text-gray-700">Email address</span>
-                                    <input
-                                        type="text"
-                                        class="
-                                            mt-1
-                                            block
-                                            w-full
-                                            rounded-md
-                                            border-gray-300
-                                            shadow-sm
-                                            focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
-                                        "
-                                        placeholder=""
-                                    />
-                                </label>
-                                <label class="block">
-                                    <span class="font-inter text-gray-700">Password</span>
-                                    <input
-                                        type="text"
-                                        class="
-                                            mt-1
-                                            block
-                                            w-full
-                                            rounded-md
-                                            border-gray-300
-                                            shadow-sm
-                                            focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
-                                        "
-                                        placeholder=""
-                                    />
-                                </label>
-                                <label class="block">
-                                    <span class="font-inter text-gray-700">Confirm Password</span>
-                                    <input
-                                        type="text"
-                                        class="
-                                            mt-1
-                                            block
-                                            w-full
-                                            rounded-md
-                                            border-gray-300
-                                            shadow-sm
-                                            focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50
-                                        "
-                                        placeholder=""
-                                    />
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="container mx-auto flex justify-end">
-                <button class="rounded-[4px] bg-iris text-[18px] text-white font-normal text-center py-2.5 px-5 mt-8 mr-7">ยืนยัน</button>
-            </div>
-        </form>
-    </div>
-  )
+    )
 }
