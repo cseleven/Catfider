b9e5f5115c34fcca1e6801171f98520ec73a93d7
jjjjjjj7777777 <jee3848@gmail.com>
2022-12-14 12:00:42 +0700

    form-ajs queue.js

--- frontend/pages/user/queue.js
+++ frontend/pages/user/queue.js
@@ -1,7 +1,30 @@
-function Queue() {
-    return (
-      <h2>จองคิวแมว</h2>
-    )
+import Head from 'next/head'
+import Image from 'next/image'
+import vectorHome from '../public/queue/vector-home.png'
+
+
+export default function Queue() {
+  return (
+    <div class="container">
+      <Head>
+        <title>Cat Finder</title>
+        <meta name="description" content="create by eleven" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/*section 1*/}
+      <div class="w-screen h-32">
+        <div class="flex">
+          <Image class="" src={vectorHome} placeholder="blur"/>
+
+          <p class=""></p>
+        </div>
+
+      </div>
+
+
+    </div>
+
+  )
 }
-  
-export default Queue
+
