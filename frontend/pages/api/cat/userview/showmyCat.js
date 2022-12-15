73777922e6d35de43f46d62360749b54461a053c
kanticha954 <kanticha.tri@gmail.com>
2022-12-15 21:48:24 +0700

    fix userview cat

--- frontend/pages/api/cat/userview/showmyCat.js
+++ frontend/pages/api/cat/userview/showmyCat.js
@@ -2,21 +2,20 @@ import { supabase } from "../../supabase"
 import { useEffect, useState } from 'react'
 
 export default async function handler(req, res) {
-    //get profile cat in shelter view per cat id
+    //get my cat in user view
     //select by shelter_id
 
     const { user_id } = req.body
 
     const { count, err } = await supabase
-        .from('queue')
+        .from('user_profile')
         .select('*', { count: 'exact', head: true })
         .match({ user_id: user_id })
 
     let query = supabase
-        .from('queue')
-        .select('cat_profile(cat_id, cat_name, sex, breed, color, cat_picture, status, create_date)')
-    if (shelter_id) { query = query.eq('shelter_id', shelter_id) }
-
+        .from('user_profile')
+        .select('user_id, queue(queue_id, cat_profile(cat_id, cat_name, sex, breed, color, cat_picture, status, shelter_profile(shelter_name)))')
+        .eq('user_id', user_id) 
 
     const { data, error } = await query
 
