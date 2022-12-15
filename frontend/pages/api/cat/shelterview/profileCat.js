ab2c9b55b24431b2f341e68fb73ac6a936b618d9
kanticha954 <kanticha.tri@gmail.com>
2022-12-15 20:31:35 +0700

    edit profileCat-myCat Shelter view

--- frontend/pages/api/cat/shelterview/profileCat.js
+++ frontend/pages/api/cat/shelterview/profileCat.js
@@ -2,20 +2,14 @@ import { supabase } from "../../supabase"
 import { useEffect, useState } from 'react'
 
 export default async function handler(req, res) {
-    //select by shelter_id
-    const { shelter_id } = req.body
-
-    const { count, err } = await supabase
-        .from('cat_profile')
-        .select('*', { count: 'exact', head: true })
-        .match({ shelter_id: shelter_id })
-
-
+    //get profile cat in shelter view per cat id
+    //select by cat_id
+    const { cat_id } = req.body
     let query = supabase
         .from('cat_profile')
         //.select('cat_id, cat_name, detail, age, sex, breed, color, sterile, vaccine, status, cat_picture, shelter_id, shelter_profile!inner(shelter_name), queue!inner(queue_date, user_id, queue_status), user_profile!inner(user_id, user_email)')
-        .select('cat_id, cat_name, detail, age, sex, breed, color, sterile, vaccine, status, cat_picture, shelter_id, shelter_profile!inner(shelter_name, donate_name1, donate_number1, donate_name2, donate_number2), queue!inner(queue_date, user_id, queue_status)')
-        if (shelter_id) { query = query.eq('shelter_id', shelter_id) }
+        .select('cat_id, cat_name, detail, age, sex, breed, color, sterile, vaccine, status, cat_picture, shelter_id, shelter_profile!inner(shelter_name, donate_name1, donate_number1, donate_name2, donate_number2), queue!inner(queue_date, user_id, user_profile!inner(user_email), queue_status)')
+        if (cat_id) { query = query.eq('cat_id', cat_id) }
 
     //more ช่องทางการบริจาค + user email
 
@@ -24,7 +18,6 @@ export default async function handler(req, res) {
     if (error) {
         throw error
     }
-    console.log(count)
     res.status(200).json(data)
 
 }
