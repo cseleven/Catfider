c04ac514a6184942a104181cb47cab76a76565e6
kanticha954 <kanticha.tri@gmail.com>
2022-12-15 20:12:10 +0700

    fix profileCat/getShelter

--- frontend/pages/api/cat/shelterview/profileCat.js
+++ frontend/pages/api/cat/shelterview/profileCat.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
     let query = supabase
         .from('cat_profile')
         //.select('cat_id, cat_name, detail, age, sex, breed, color, sterile, vaccine, status, cat_picture, shelter_id, shelter_profile!inner(shelter_name), queue!inner(queue_date, user_id, queue_status), user_profile!inner(user_id, user_email)')
-        .select('cat_id, cat_name, detail, age, sex, breed, color, sterile, vaccine, status, cat_picture, shelter_id, shelter_profile!inner(shelter_name), queue!inner(queue_date, user_id, queue_status)')
+        .select('cat_id, cat_name, detail, age, sex, breed, color, sterile, vaccine, status, cat_picture, shelter_id, shelter_profile!inner(shelter_name, donate_name1, donate_number1, donate_name2, donate_number2), queue!inner(queue_date, user_id, queue_status)')
         if (shelter_id) { query = query.eq('shelter_id', shelter_id) }
 
     //more ช่องทางการบริจาค + user email
--- frontend/pages/api/shelter/getShelter.js
+++ frontend/pages/api/shelter/getShelter.js
@@ -9,8 +9,9 @@ export default async function handler(req, res) {
   var shelter = await checkShelterId(shelter_id)
   if (shelter) {
     //query
-    const { data } = await supabase.from('shelter_profile').select('shelter_name, website_url, detail, contact_email, contact_phone, address, location_url, contact_name, contact_lastname, cover_picture, profile_picture')
+    const { data } = await supabase.from('shelter_profile').select()
     .eq('shelter_id', shelter_id)
+    console.log("SELECT SHELTER SUCESS")
     res.status(200).json(data)
   } else {
     res.status(400).json("Shelter ID Not Found")
@@ -20,7 +21,7 @@ export default async function handler(req, res) {
   //check user_id exist
 async function checkShelterId(shelter_id, response) {
   //query
-  const { data, error } = await supabase.from('shelter_profile').select().eq('shelter_id', shelter_id)
+  const { data, error } = await supabase.from('shelter_profile').select('*').eq('shelter_id', shelter_id)
   if ( data == "" ) {
     //shelter_id does not exist
     response = false
