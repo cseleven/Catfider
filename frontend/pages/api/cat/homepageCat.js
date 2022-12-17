a212474e09302a15ec830b74d31be9c11199eec4
kanticha954 <kanticha.tri@gmail.com>
2022-12-18 01:51:03 +0700

    homepage api link

--- frontend/pages/api/cat/homepageCat.js
+++ frontend/pages/api/cat/homepageCat.js
@@ -1,13 +1,16 @@
-import { supabase } from "../../supabase"
+import { supabase } from "../supabase"
 import { useEffect, useState } from 'react'
 
 export default async function handler(req, res) {
     //show 3 in home page
+    const { number } = req.body
 
     let query = supabase
         .from('cat_profile')
-        .select('cat_id, cat_name, detail, sex, breed, color, cat_picture, shelter_id')
-        .limit(3)
+        .select('cat_id, cat_name, detail, sex, breed, color, cat_picture, status, shelter_id, shelter_profile(shelter_name)')
+        .eq("status", false)
+        .order("cat_id")
+        .limit(number)
 
     const { data, error } = await query
 
@@ -15,6 +18,7 @@ export default async function handler(req, res) {
         throw error
     }
 
+    console.log("Homepage Cat Show")
     console.log(data)
     res.status(200).json(data)
 
